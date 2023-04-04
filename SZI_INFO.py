04ae8d8206847de1dbb41664f67900aa1874d9c2
yxungflare <yxungflare@yandex.ru>
2023-04-04 12:46:31 +0300

    some updates

--- SZI_INFO.py
+++ SZI_INFO.py
@@ -1,16 +1,22 @@
-class FDataBase:
-       def __init__(self, db):
-              self.__db = db
-              self.__cur = db.cursor()
+# class FDataBase:
+#        def __init__(self, db):
+#               self.__db = db
+#               self.__cur = db.cursor()
 
-       def getMenu(self):
-              sql = '''SELECT * FROM Models'''
-              try:
-                     self.__cur.execute(sql)
-                     res = self.__cur.fetchall()
-                     result = list(map(list, res))
-                     if res:
-                            return result
-              except:
-                     print("Ошибка чтения из БД")
-              return []
+#        def getMenu(self):
+#               sql = f'''SELECT * FROM Models'''
+#               try:
+#                      self.__cur.execute(sql)
+#                      res = self.__cur.fetchall()
+#                      result = list(map(list, res))
+#                      if res:
+#                             return result
+#               except:
+#                      print("Ошибка чтения из БД")
+#               return []
+
+
+SZI_models = [
+    ['Sobol', '62000', 'Несанкционированный доступ', '0.8'],
+    ['SecretDisk', '45000', 'Подтверждение авторства', '0.9']
+]
--- page3.py
+++ page3.py
@@ -1,40 +1,13 @@
 from flask import Flask, request, render_template, g
-from SZI_INFO import FDataBase
-import sqlite3
+from SZI_INFO import SZI_models
 import os
 
-DATABASE = 'SZI.db'
 SECRET_KEY = 'ffrfrmeknsdnsvkjk3nbhjzzzz'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-# Переопределяем путь к бд
-app.config.update(dict(DATABASE=os.path.join(app.root_path, 'SZI.db')))
-
-
-def connect_db():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def create_db():
-    db = connect_db()
-    with app.open_resource('create_table.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-    db.close()
-
-
-def get_db():
-    # Соединение с бд, если не было установлено
-    if not hasattr(g, 'link_db'):
-        g.link_db = connect_db()
-    return g.link_db
-
-
 models = []
-
 
 @app.route("/")
 def index():
@@ -44,16 +17,10 @@ def index():
 # Обработка СЗИ
 @app.route("/", methods=['POST', 'GET'])
 def choose_model():
-    db = get_db()
-    dbase = FDataBase(db)
-    print(dbase.getMenu())
-    if request.method == 'POST':
-        SZI1 = request.form.get('model')
-        SZI2 = request.form.get('model2')
-        if SZI1:
-            model = SZI1
-        if SZI2:
-            model = SZI2
+    if 'model' in request.form:
+        model = request.form.getlist('model')[-1]
+    elif 'model2' in request.form:
+        model = request.form.getlist('model2')[-1]
 
         if model not in models:
             models.append(model)
@@ -68,15 +35,8 @@ def choose_model():
                     if m != model and m in ['Rutoken', 'PKIClient', 'Cryptopro', 'VipNet', 'SecretDisk']:
                         del models[models.index(m)]
             print(models)
-            
+
         return render_template('page3.html', models=models, model=model)
-
-
-@app.teardown_appcontext
-def close_db(error):
-    # разрываем соединение, если его не было
-    if hasattr(g, 'link_db'):
-        g.link_db.close()
 
 
 @app.route("/page2.html")
